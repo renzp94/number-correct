bb9f5def664a4a3b88df7cb2f79f76d6602c00d6
renzp94 <1074720760@qq.com>
2023-10-10 10:12:40 +0800

    refactor: 整理代码，抽离公共逻辑代码

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,59 @@
-import { VNumber } from './vMath'
+import type { VData, VNumber } from './vMath'
 
+/**
+ * 将数字字符串转换成数字数组
+ * @param v 数字字符串
+ * @returns
+ */
 export const transformNumberArray = (v: string) =>
   v?.split('')?.map(Number) ?? []
-
-export const createGetMaxLen = (filed: 'integer' | 'decimal') => {
+/**
+ * 创建一个获取数组中元素最大长度值的reduce函数
+ * @param filed 属性
+ * @returns reduce函数
+ */
+export const createGetMaxLenReduce = (filed: 'integer' | 'decimal') => {
   return (prev: number, curr: VNumber) => {
     const len = curr[filed].length
     return len > prev ? len : prev
   }
+}
+/**
+ * 创建数字数组
+ * @param length 数组长度
+ * @returns
+ */
+export const createNumberArray = (length: number) =>
+  Array.from({ length }).fill(0) as number[]
+/**
+ * 拼接整数数组和小数数组为一个数
+ * @param integer 整数数组
+ * @param decimal 小数数组
+ * @returns 数值的字符串
+ */
+export const joinNumber = (integer: number[], decimal: number[]) => {
+  // 计算前将所有位反转了，此处先反转再拼接
+  let number = integer.reverse().join('')
+  // 如果有小数则拼接小数
+  const hasDecimal = decimal.some((item) => item)
+  if (hasDecimal) {
+    number += `.${decimal.reverse().join('')}`
+  }
+
+  return number
+}
+/**
+ * 将VData类型的数据整合
+ * @param target 数据源
+ * @param computed 计算函数
+ * @returns 返回最终数据数组
+ */
+export const reduceVData = (
+  target: VData,
+  computed: (prev: number[], curr: number[]) => string,
+) => {
+  return target.data.reduce(
+    (prev, curr) => transformNumberArray(computed(prev, curr)).reverse(),
+    target.defaultData,
+  )
 }
--- src/vMath.ts
+++ src/vMath.ts
@@ -1,3 +1,9 @@
+import {
+  createGetMaxLenReduce,
+  createNumberArray,
+  transformNumberArray,
+} from './utils'
+
 type Operation = (
   curr: number,
   next: number,
@@ -22,7 +28,7 @@ const createVCalc = (operation: Operation) => {
     const currCount = currList?.length ?? 0
     const nextCount = nextList?.length ?? 0
     const count = currCount > nextCount ? currCount : nextCount
-    let result = Array.from({ length: count }).fill(0) as number[]
+    let result = createNumberArray(count)
 
     let index = 0
     do {
@@ -37,25 +43,23 @@ const createVCalc = (operation: Operation) => {
 }
 /**
  * 加法实现
+ * @param curr 当前值
+ * @param next 下一个值
+ * @param result 初始数组
+ * @param index 初始数组当前下标
+ * @returns 相加之后的数组
  */
 const _plus = (curr: number, next: number, result: number[], index: number) => {
-  let [unit, ten] = (curr + next)
-    .toString()
-    // 拆分成数组用于进位
-    .split('')
-    // 反转以处理没有十位的情况
-    .reverse()
-    .map(Number)
+  let [unit, ten] = transformNumberArray((curr + next).toString()).reverse()
 
   // console.log(`${curr} + ${next} = ${ten ?? ''}${unit}`)
   // 将上一位的进位加上，如果没有进位则加0
   result[index] = result[index] + unit
   // 拆分当前计算位是否需要进位，拆分了之后才是当前位计算的结果
-  const [indexUnit, indexTen] = result[index]
-    .toString()
-    .split('')
-    .map(Number)
-    .reverse()
+  const [indexUnit, indexTen] = transformNumberArray(
+    result[index].toString(),
+  ).reverse()
+
   unit = indexUnit
   // 如果当前位在未拆分前计算的结果就已经进位则无需使用拆分后的进位值
   ten = ten ?? indexTen
@@ -101,3 +105,68 @@ const _minus = (
 
 export const vPlus = createVCalc(_plus)
 export const vMinus = createVCalc(_minus)
+
+export interface VData {
+  data: number[][]
+  defaultData: number[]
+  maxLen: number
+}
+
+/**
+ * 获取竖式计算的数据
+ * @param numbers 相加数值数组
+ * @returns [整数数据，小数数据]
+ */
+export const getVData = (numbers: Array<string | number>): [VData, VData] => {
+  // 拆分整数和小数
+  let list: VNumber[] = numbers.map((v) => {
+    const [integer, decimal] = v.toString().split('.')
+
+    return {
+      integer: transformNumberArray(integer),
+      decimal: transformNumberArray(decimal),
+    }
+  })
+
+  // 整数最大位数
+  const integerMaxLen = list.reduce(createGetMaxLenReduce('integer'), 0)
+  // 小数最大位数
+  const decimalMaxLen = list.reduce(createGetMaxLenReduce('decimal'), 0)
+
+  list = list.map((item) => {
+    // 整数一定有，根据最大位数用0补齐所有位数
+    const integerZeroList = createNumberArray(
+      integerMaxLen - item.integer.length,
+    )
+    // 整数要先反转再补位
+    item.integer = item.integer.reverse().concat(integerZeroList)
+    // 小数如果有也根据最大位数用0补齐所有位数
+    if (decimalMaxLen > 0) {
+      const decimalZeroList = createNumberArray(
+        decimalMaxLen - item.decimal.length,
+      )
+      item.decimal = item.decimal.concat(decimalZeroList).reverse()
+    }
+    return item
+  })
+
+  const decimalList = list.map((item) => item.decimal)
+  const defaultDecimalList = createNumberArray(decimalMaxLen)
+
+  const integerList = list.map((item) => item.integer)
+  // 少一位补0，用于空出进位位置
+  const defaultIntegerList = createNumberArray(integerMaxLen - 1)
+
+  return [
+    {
+      data: integerList,
+      defaultData: defaultIntegerList,
+      maxLen: integerMaxLen,
+    },
+    {
+      data: decimalList,
+      defaultData: defaultDecimalList,
+      maxLen: decimalMaxLen,
+    },
+  ]
+}
